c9e2cc5f6c2d3c013cdda3b65be7ef3f029e0a17
Meryem Dibe <dibemeryem@gmail.com>
2023-10-07 13:06:50 +0100

    Add: Aliases

--- src/sandbox.ts
+++ src/sandbox.ts
@@ -1,77 +1,20 @@
-// Explicit types 
-let character :string;
-let age :number;
-let isLoggedIn :boolean;
-console.log("test");
+// Aliases
+type StrOrNum = number | string;
+type ObjWith = { uid: StrOrNum; item: string }
 
-// arrays
-let table: string[];
+let logDetail = (uid: StrOrNum,  item: string) => {
+  console.log(`${item} has uid of ${undefined} `);
+};
 
-    // cant do table.push('hello');
+// function with an object parameter
+let greet = (user: ObjWith , phrase: string) => {
+  console.log(` ${user.item} with ${user.uid} says ${phrase}  `);
+};
 
-    // need to initialise array 
-    let ninjas: string[] = [];
-    ninjas.push('hello world');
-    console.log("string array",ninjas);
+let obj : ObjWith
+obj = {
+  uid: "122SRTR",
+  item: "Ssam"
+};
 
-//Union types for different types to a variable
-    // Array variable needs parantheses
-    let mixed: (string|boolean|number) [] = [];
-    mixed.push('hello');
-    mixed.push(33);
-    mixed.push(true);
-    console.log("mixed", mixed);
-
-    // variable 
-
-    let uuid : string|number;
-    uuid = 30;
-    console.log("uuid with string|number types ", uuid);
-    uuid = "xxxx"; 
-    console.log("uuid with string|number types ", uuid);
-
-// Declaring objects
-let ninjaOne : object ;
-
-    // in this case we have a large choice in our object and unlimited props 
-    ninjaOne = { name : "test", age: 40}
-    ninjaOne = { title : "test", course: "IT"}
-
-    // we can create array out of it because array is kind of an object
-    ninjaOne = [];
-
-    // if you want to be more specific (remember we use colon : not =)
-    let ninjatwo: {
-        name: string,
-        age:number
-    };
-
-    // cannot do just ninjatwo: {}
-    // the oject should declare all props no more no less
-
-
-// any type used mostly when we dont know the type of the variable 
-
-    let test: any = 23;
-    test = "Meryem";
-    test = true;
-
-    let mixedArray : any[] = [];
-    mixedArray.push('test');
-    mixedArray.push(22);
-    mixedArray.push(false);
-
-    console.log("mixedArray with any type", mixedArray);
-
-    let obj: {
-        name: any,
-        age: any
-    }
-    obj = {
-        name : "mery",
-        age: 22
-    }
-    obj = {
-        name : true,
-        age: "Not specified"
-    }
+greet(obj, "Hello");
